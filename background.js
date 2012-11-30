591cbfb69a3cd142f2597df5fd6559b939cfb9c5
Doug Feigelson <dougfeig@gmail.com>
2012-11-30 07:51:12 -0500

    button functionality

--- background.js
+++ background.js
@@ -2,33 +2,45 @@ var disabled = false;
 // Called when the user clicks on the browser action.
 chrome.browserAction.onClicked.addListener(function(tab) {
     disabled = !disabled;
-    chrome.tabs.executeScript(null, {code:"document.body.style.background='red !important'"});
-});
+    console.log("DISABLED: ",disabled);
 
+    if (disabled) {
+        // get all tabs and KILL minimaps in them
+        chrome.tabs.query({}, function(tabs) {
+            for (var i = 0; i < tabs.length; i++) {
+                var tab = tabs[i];
+                chrome.tabs.executeScript(tab.id, {code: "$('#minimap').remove();"}, null);
+            }
+        });
+        return;
+    }
+    // could try to remake minimaps on all tabs here, but a quick attempt revealed many issues
+});
 
 function loadScripts(tabID, scriptArray){
     if (scriptArray.length > 0){
-	var scriptName = scriptArray[0];
-	chrome.tabs.executeScript(tabID, {file: scriptName}, function(result){
-	    console.log("loaded "+scriptName);
-	    loadScripts(tabID, scriptArray.slice(1));
-	});
+    var scriptName = scriptArray[0];
+    chrome.tabs.executeScript(tabID, {file: scriptName}, function(result){
+        console.log("loaded "+scriptName);
+        loadScripts(tabID, scriptArray.slice(1));
+    });
     } else {
-	console.log("all scripts loaded.");
+    console.log("all scripts loaded.");
     }
 }
 
 function createMinimap(tab){
     console.log("creating minimap");
-    if (disabled) {return;};
+    if (disabled) {return;}
     loadScripts(tab.id, ["minimap.js"]);
 }
 
-
 chrome.tabs.onUpdated.addListener(function(tabID){
     chrome.tabs.get(tabID, function(tab){
-	if (tab.status === "complete" && (tab.url != undefined)){
-	    createMinimap(tab);
-	}
+        if (tab.status === "complete" && (tab.url !== undefined)){
+            createMinimap(tab);
+        }
     });
 });
+
+

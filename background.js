92e9f697f4cf84487f8a6ea41e674836605ac249
Ethan Sherbondy <sherbondye@gmail.com>
2012-11-29 17:38:12 -0800

    progress

--- background.js
+++ background.js
@@ -1,3 +1,22 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
+function createMinimap(){
+    console.log("done loading.");    
+}
+
+function updateMinimap(){
+}
+
+function moveViewport(){
+}
+
+chrome.browserAction.onClicked.addListener(function(tabID) {
+    // toggle the minimap here.
     chrome.tabs.executeScript(null, {code:"alert('hooray.');"});
+});
+
+chrome.tabs.onUpdated.addListener(function(tabID){
+    chrome.tabs.get(tab, function(tab){
+	if (tab.status === "complete" && (tab.url != undefined)){
+	    createMinimap();
+	}
+    });
 });

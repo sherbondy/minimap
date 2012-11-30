edf8351ca8e0594dcd1ec88f5543116a82d63cd9
Ethan Sherbondy <sherbondye@gmail.com>
2012-11-29 18:11:10 -0800

    load css and all deps.

--- minimap.js
+++ minimap.js
@@ -1,1 +1,10 @@
-console.log("WOOHOO.");
+var minimap = 
+    '<div id="minimap">'+
+    '    <canvas id="minimap-canvas"></canvas>'+
+    '</div>';
+
+function createMinimap(){
+    console.log("hi");
+}
+
+createMinimap();

578c015b45e0b5e88818fef72f3d53a83a86e8e8
Doug Feigelson <dougfeig@gmail.com>
2012-11-30 10:42:04 -0500

    work

--- minimap.js
+++ minimap.js
@@ -102,32 +102,13 @@ function updatePageCanvas(){
         return;
     }
 
-    html2canvas( [ document.body ], {
-                // general
-        logging: false,
 
-        // preload options
-        proxy: false,
-        timeout: 0,    // no timeout
-        useCORS: false, // try to load images as CORS (where available), before falling back to proxy (not yet implemented?)
-        allowTaint: false, // whether to allow images to taint the canvas, won't need proxy if set to true
-
-        // parse options
-        svgRendering: false, // use svg powered rendering where available (FF11+)
-        iframeDefault: "default",
-        ignoreElements: "IFRAME|OBJECT|PARAM",
-        useOverflow: true,
-        letterRendering: false,
-
-        // render options
-
-        flashcanvas: undefined, // path to flashcanvas
-        width: null, // TODO: set size here instead?
-        height: null,
-        taintTest: true, // do a taint test with all images before applying to canvas
-        renderer: "Canvas",
-        onrendered: canvasRendered
-    });
+    hc = new IgeHtmlCanvas();
+    
+    hc.initContainer();
+    hc.loadView('../views/test1.html', '../views/test1.css');
+    
+    hc.renderHtml();
 }
 
 function scrollHandler() {
